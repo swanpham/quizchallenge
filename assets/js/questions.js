05beff709f37b54a33e3691297efbbc9a5ebd656
Swan Pham <accounting@nailsrus.ca>
2021-03-20 22:46:14 -0400

    index.html

---
+++ assets/js/questions.js
@@ -0,0 +1,63 @@
+// Creating an array and passing the number, question, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+            
+    },
+    {
+        numb: 2,
+        questions: "After you’re done creating and testing a new feature in a feature branch, what is the next step?",
+        answer: "Merge the feature branch into the main branch",
+        options: [
+            "Merge the feature branch into the main branch",
+            "Merge the feature branch into the develop branch",
+            "Create a new feature branch",
+            "Merge a new feature branch",
+        ]
+            
+    },
+    {
+        numb: 3,
+        questions: "How do you create a flexbox?",
+        answer: "display: flex;",
+        options: [
+            "display: flex;",
+            "display: flexbox;",
+            "display: box;",
+            "display: 0;",
+        ]
+            
+    },
+    {
+        numb: 4,
+        questions: "By default, in which direction does a flexbox lay out its items?",
+        answer: "A row (horizontal), with all of the child elements laid out side by side.",
+        options: [
+            "A row (horizontal), with all of the child elements laid out side by side.",
+            "A column (vertical), with all of the child elements laid out on top of one another.",
+            "A column (vertical), with no child elements laid out on top of one another.;",
+            "A row (horizontal), with no of the child elements laid out side by side.",
+        ]
+            
+    },
+    {
+        numb: 5,
+        questions: "What do media queries allow us to do?",
+        answer: "Change CSS at different browser widths.",
+        options: [
+            "Play videos on our page.",
+            "Create responsive designs.",
+            "Change CSS at different browser widths.",
+            "chane a new page",
+        ]
+            
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,97 @@
 //getting all required elements
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart");
-const quiz_box = info_box.querySelector("quiz_box");
+const exit_btn = document.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".continue ");
+const quiz_box = document.querySelector(".quiz_box");
+const questionEl = document.querySelector("#questionbox");
 
 //If start Quiz Button Clicked
 start_btn.onclick = ()=> {
-    info_box.classList.add("activeInfo");//show the info box
+    // info_box.classList.add("activeInfo");//show the info box
+    // start_btn.style.display = "none"
+    info_box.style.display = "block"
+    console.log(questions)
 }
 
 //If Exit Button Clicked
 exit_btn.onclick = ()=> {
-    info_box.classList.remove("activeInfo");//hide the info box
+    // info_box.classList.remove("activeInfo");//hide the info box
+    start_btn.style.display = "block"
+    info_box.style.display = "none"
 }
+const question = "What does HTML stand for?"
+
 //If continue Button Clicked
 continue_btn.onclick = ()=> {
-    info_box.classList.remove("activeInfo");//hide the info box
-    quiz_box.classList.remove("activeQuiz");//show the quiz box
+    info_box.style.display = "none"
+    quiz_box.style.display = "block"
+    questionEl.innerHTML = `<div class="ques_text">
+   ${question}
+</div>
+<div class="option_list">
+    <div class="option">
+        <span>Hyper Text Preprocessor</span>
+        <div class="icon tick"><i class="fas fa-check"></i></div>
+    </div>
+    <div class="option">
+        <span>Hyper Text Markup Language</span>
+        <div class="icon cross"><i class="fas fa-times"></i></div>
+    </div>
+    <div class="option">
+        <span>Hyper Text Multiple Language</span>
+    </div>
+    <div class="option">
+        <span>Hyper Tool Multi Language</span>
+    </div>
+</div>`
+    // info_box.classList.remove("activeInfo");//hide the info box
+    // quiz_box.classList.add("activeQuiz");//show the quiz box
+//     showQuestions(0);
+//    queCounter(1);
+    console.log("hello");  
 }
+
+
+let que_count = 0;
+let que_numb = 1;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+// If Next Button Clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.lenght -1) {
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(1);
+    }else {
+        console.log("questions completed");
+    }
+}
+
+// Getting questions and options from array
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    // let que_tag = '<span>'+ questions[index].numb + "." + question +'<span>';
+    let option_tag = '<div class="option">' + questions[index].options[0] +'<span></span></div>'
+                     + '<div class="option">' + questions[index].options[1] +'<span></span></div>'
+                     + '<div class="option">' + questions[index].options[2] +'<span></span></div>'
+                     + '<div class="option">' + questions[index].options[3] +'<span></span></div>';                
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.lenght; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correctAns = 
+    console.log(userAns);
+    
+}
+
